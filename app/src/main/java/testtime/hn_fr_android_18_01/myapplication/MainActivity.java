c6d21d8027844a4aba085ad43c41ec43413bd34a
heroheretic <heroheretic@gmail.com>
2018-05-05 20:08:41 +0700

    reup

---
+++ app/src/main/java/testtime/hn_fr_android_18_01/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package testtime.hn_fr_android_18_01.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
